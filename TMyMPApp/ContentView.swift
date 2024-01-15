57fb173d6d22291d0518163e35386b1f7d1a72ec
Thorsten Hindermann <hindermath@googlemail.com>
2024-01-15 12:27:50 +0100

    Initial Commit

---
+++ TMyMPApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TMyMPApp
+//
+//  Created by Thorsten Hindermann on 15.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TMyMPApp/TMyMPAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMyMPAppApp.swift
+//  TMyMPApp
+//
+//  Created by Thorsten Hindermann on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct TMyMPAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
